1c5f5b73af90c4d91aa12925e21ac720b2eed65b
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-24 21:13:00 -0400

    adding a slugify method for the resort model that runs every time we crete a new resort before we add it to our database

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,2 +1,12 @@
 class Resort < ApplicationRecord
+    has_many :reviews
+
+    #set the slug for the resort before we add it to our database
+    before_create :slugify
+
+    def sligify
+        self.slug = name.parameterize
+    end
 end
+
+# add a method for when we create a new resort we'll take the name of that resort and we'll sligify it and then set that as the slug value before we then save that new resort in our database. It takes the resort name and converts it into a lower case hyphen name and sets is as a slug value
