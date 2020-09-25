3ecf24dee8e52532f99bf5387637da97e2f47163
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-25 15:11:14 -0400

    added the function to get an average score of the place based on 2 reviews

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -4,9 +4,15 @@ class Resort < ApplicationRecord
     #set the slug for the resort before we add it to our database
     before_create :slugify
 
-    def sligify
+    def slugify
         self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.average(:score).round(2).to_f
     end
 end
 
-# add a method for when we create a new resort we'll take the name of that resort and we'll sligify it and then set that as the slug value before we then save that new resort in our database. It takes the resort name and converts it into a lower case hyphen name and sets is as a slug value
+# SLUGIFY METHOD add a method for when we create a new resort we'll take the name of that resort and we'll sligify it and then set that as the slug value before we then save that new resort in our database. It takes the resort name and converts it into a lower case hyphen name and sets is as a slug value
+
+# AVD_SCORE is getting the average score of the review rounded up and converted to a float
