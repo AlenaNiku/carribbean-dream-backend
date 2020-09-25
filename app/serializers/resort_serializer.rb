5d9ccf2c458373b4905c2336c634926693a5551c
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-25 16:04:29 -0400

    adding the routes for the api

--- app/serializers/resort_serializer.rb
+++ app/serializers/resort_serializer.rb
@@ -1,4 +1,6 @@
 class ResortSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :image_url, :slug
+
+  has_many :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :resorts, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  # route requests that are not for existing paths predefined in our api back to our index path (will handle routing to our react components without interfering with rails routes for our api)
+  get '*path', to: 'pages#index', via: :all
 end
+
+# specifying param: :slug for the resorts resources allows us to use the slug value that we created instead of using the id
