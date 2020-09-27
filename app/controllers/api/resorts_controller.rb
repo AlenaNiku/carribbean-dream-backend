29108661a9e4d9399d3b61fe8063edeb72493d81
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-27 13:18:53 -0400

    added a show method for rendering an individual resort

--- app/controllers/api/resorts_controller.rb
+++ app/controllers/api/resorts_controller.rb
@@ -1,10 +1,18 @@
 module Api
     module V1
         class ResortsController < ApplicationController
+            #rendering all resorts
             def index
                 resorts = Resort.all 
 
                 render json: ResortSerializer.new(resorts).serialized_json
+            end
+
+            #rendering individual resort
+            def show
+                resort = Resort.find_by(slug: params[:slug])
+
+                render json: ResortSerializer.new(resort).serialized_json
             end
         end
     end
