31525857d77987150ccde7446f918f681dba0e38
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-27 13:12:41 -0400

    adding index method for rendering all resorts

--- app/controllers/api/resorts_controller.rb
+++ app/controllers/api/resorts_controller.rb
@@ -1,6 +1,11 @@
 module Api
     module V1
         class ResortsController < ApplicationController
+            def index
+                resorts = Resort.all 
+
+                render json: ResortSerializer.new(resorts).serialized_json
+            end
         end
     end
 end
