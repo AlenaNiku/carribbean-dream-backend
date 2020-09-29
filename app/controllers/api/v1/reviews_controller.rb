72c415a3fd705c5494956d22bca15b55d1754ad2
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-29 13:57:47 -0400

    added a desrtoy method to the resorts controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,6 +2,7 @@ module Api
     module V1
         class ReviewsController < ApplicationController
 
+            # POST /api/v1/reviews
             def create
                 review = Review.new(review_params)
 
@@ -12,8 +13,20 @@ module Api
                 end
             end
 
+            # DELETE /api/v1/reviews/:id
+            def destroy
+                review = Review.find(params[:id])
+
+                if review.destroy
+                    head :no_content
+                else
+                    render json: { error: review.errors.messages }, status: 422
+                end
+            end
+
             private
 
+            # strong params
             def review_params
                 params.require(:review).permit(:title, :description, :score, :resort_id)
             end
