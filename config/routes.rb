230138ba36bf2ddc72a920cd0e47b2d75b53dcfc
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-30 14:08:56 -0400

    changed format of reviews resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  
   root 'pages#index'
 
   namespace :api do
     namespace :v1 do
       resources :resorts, param: :slug
-      resources :reviews, only: [:create, :destroy]
+      resources :reviews, only: %i[create destroy]
     end
   end
 
